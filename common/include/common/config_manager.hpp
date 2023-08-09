153e4f3f5a0d2e36163ad3f8296cbba16ebfb6fc
ccamel55 <allanma31@gmail.com>
2023-08-10 02:10:54 +1200

    config: working on config manager

---
+++ common/include/common/config_manager.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+#include <common/types/hash/fnv1a.hpp>
+#include <nlohmann/json.hpp>
+
+namespace lib::common
+{
+	//! Config manager using json.
+	class config_manager
+	{
+	public:
+		config_manager(const std::filesystem::path& config_directory, const std::string& file_extension);
+
+		//! Create a new config in the config directory with \a config_name.
+		void create_config(const std::string& config_name);
+
+		//! Attempt to remove the config named \a config_name.
+		void remove_config(const std::string& config_name);
+
+		//! Attempt to save the config named \a config_name.
+		void save_config(const std::string& config_name);
+
+		//! Attempt to load the config named \a config_name.
+		void load_config(const std::string& config_name);
+
+		//! Attempt get all the configs in the current config directory.
+		void refresh_config_list();
+
+	private:
+
+	};
+}
