54f69b0e365c39036287151cbf7650010afcbd36
ccamel55 <allanma31@gmail.com>
2023-10-15 01:40:24 +1300

    unit tests: impl some math and crypto tests

--- unit_test/cryptography_tests.cpp
+++ unit_test/cryptography_tests.cpp
@@ -1,6 +1,55 @@
 #include <catch2/catch_test_macros.hpp>
+#include <common/hash/fnv1a.hpp>
 
-TEST_CASE("test1")
+namespace hash = lib::common;
+
+TEST_CASE("FNV-1A", "[hashing]")
 {
-	REQUIRE(0 == 0);
+	constexpr auto test_string_1 = "hello poo poo";
+	constexpr auto expected_hash_1 = 0x6c068f17;
+
+	constexpr auto test_string_2 = "testtest";
+	constexpr auto expected_hash_2 = 0xfb19d205;
+
+	SECTION("compile time evaluation")
+	{
+		REQUIRE(hash::fnv1a::get_compile_time(test_string_1) == expected_hash_1);
+		REQUIRE(hash::fnv1a::get_compile_time(test_string_2) == expected_hash_2);
+
+		REQUIRE(hash::fnv1a::get_compile_time(test_string_1) != hash::fnv1a::get_compile_time(test_string_2));
+	}
+
+	SECTION("run time evaluation")
+	{
+		REQUIRE(hash::fnv1a::get(test_string_1) == expected_hash_1);
+		REQUIRE(hash::fnv1a::get(test_string_2) == expected_hash_2);
+
+		REQUIRE(hash::fnv1a::get(test_string_1) != hash::fnv1a::get(test_string_2));
+	}
+}
+
+TEST_CASE("CRC-32 - todo", "[hashing]")
+{
+	SECTION("compile time evaluation")
+	{
+		REQUIRE(true);
+	}
+
+	SECTION("run time evaluation")
+	{
+		REQUIRE(true);
+	}
+}
+
+TEST_CASE("MD-5 - todo", "[hashing]")
+{
+	SECTION("compile time evaluation")
+	{
+		REQUIRE(true);
+	}
+
+	SECTION("run time evaluation")
+	{
+		REQUIRE(true);
+	}
 }
--- unit_test/math_tests.cpp
+++ unit_test/math_tests.cpp
@@ -1,6 +1,105 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("test2")
+#include <common/types/point/point2D.hpp>
+#include <common/types/point/point3D.hpp>
+#include <common/types/point/point4D.hpp>
+
+#include <common/types/vector/vector2D.hpp>
+#include <common/types/vector/vector3D.hpp>
+
+namespace types = lib::common;
+
+TEST_CASE("Points", "[math]")
 {
-	REQUIRE(0 == 1);
+	types::point2Di point_2d_i_1(1, 2);
+	types::point2Di point_2d_i_2(3.0, 4.0);
+	types::point2Df point_2d_f_1(1, 2);
+	types::point2Df point_2d_f_2(3.0, 4.0);
+
+	types::point3Di point_3d_i_1(1, 2, 3);
+	types::point3Di point_3d_i_2(1.0, 2.0, 3.0);
+	types::point3Df point_3d_f_1(1, 2, 3);
+	types::point3Df point_3d_f_2(1.0, 2.0, 3.0);
+
+	types::point4Di point_4d_i_1(1, 2, 3, 4);
+	types::point4Di point_4d_i_2(1.0, 2.0, 3.0, 4.0);
+	types::point4Df point_4d_f_1(1, 2, 3, 4);
+	types::point4Df point_4d_f_2(1.0, 2.0, 3.0, 4.0);
+
+	SECTION("creation")
+	{
+		// only test float and integer variants, they all use the same template code so if one works the other
+		// will also likely work.
+
+		// point 2d
+		REQUIRE(point_2d_i_1._x == 1);
+		REQUIRE(point_2d_i_1._y == 2);
+
+		REQUIRE(point_2d_i_2._x == 3);
+		REQUIRE(point_2d_i_2._y == 4);
+
+		REQUIRE(point_2d_f_1._x == 1.f);
+		REQUIRE(point_2d_f_1._y == 2.f);
+
+		REQUIRE(point_2d_f_2._x == 3.f);
+		REQUIRE(point_2d_f_2._y == 4.f);
+
+		// point 3d
+		REQUIRE(point_3d_i_1._x == 1);
+		REQUIRE(point_3d_i_1._y == 2);
+		REQUIRE(point_3d_i_1._z == 3);
+
+		REQUIRE(point_3d_i_2._x == 1);
+		REQUIRE(point_3d_i_2._y == 2);
+		REQUIRE(point_3d_i_2._z == 3);
+
+		REQUIRE(point_3d_f_1._x == 1.f);
+		REQUIRE(point_3d_f_1._y == 2.f);
+		REQUIRE(point_3d_f_1._z == 3.f);
+
+		REQUIRE(point_3d_f_2._x == 1.f);
+		REQUIRE(point_3d_f_2._y == 2.f);
+		REQUIRE(point_3d_f_2._z == 3.f);
+
+		// point 4d
+		REQUIRE(point_4d_i_1._x == 1);
+		REQUIRE(point_4d_i_1._y == 2);
+		REQUIRE(point_4d_i_1._z == 3);
+		REQUIRE(point_4d_i_1._w == 4);
+
+		REQUIRE(point_4d_i_2._x == 1);
+		REQUIRE(point_4d_i_2._y == 2);
+		REQUIRE(point_4d_i_2._z == 3);
+		REQUIRE(point_4d_i_2._w == 4);
+
+		REQUIRE(point_4d_f_1._x == 1.f);
+		REQUIRE(point_4d_f_1._y == 2.f);
+		REQUIRE(point_4d_f_1._z == 3.f);
+		REQUIRE(point_4d_f_1._w == 4.f);
+
+		REQUIRE(point_4d_f_2._x == 1.f);
+		REQUIRE(point_4d_f_2._y == 2.f);
+		REQUIRE(point_4d_f_2._z == 3.f);
+		REQUIRE(point_4d_f_2._w == 4.f);
+	};
+
+	SECTION("logical operators")
+	{
+		// point 2d
+		REQUIRE(point_2d_i_1 == point_2d_i_1);
+		REQUIRE(point_2d_i_1 != point_2d_i_2);
+
+		// point 3d
+		REQUIRE(point_3d_i_1 == point_3d_i_1);
+		REQUIRE((point_3d_i_1 != point_3d_i_2) == false);
+
+		// point 4d
+		REQUIRE(point_4d_i_1 == point_4d_i_1);
+		REQUIRE((point_4d_i_1 != point_4d_i_1) == false);
+	};
+}
+
+TEST_CASE("Vectors - todo", "[math]")
+{
+
 }
