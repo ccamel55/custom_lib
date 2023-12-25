8cabc19d675920aaaaaa458e302f364cd19f8155
Allan Ma <allanma31@gmail.com>
2023-12-25 14:36:15 +1300

    refactored window creation

---
+++ components/lib_window_creation/api/glfw/window_api.cpp
@@ -0,0 +1,145 @@
+#include <lib_window_creation/window_api.hpp>
+
+#include <cassert>
+
+using namespace lib::window_creation;
+
+#if !defined(DEF_LIB_INPUT_off) && !defined(DEF_LIB_INPUT_glfw)
+// if we are using an input backend that doesn't match our window creator then need to assert
+static_assert(false, "lib_input: input backend does not match window creation backend");
+#endif
+
+namespace
+{
+// this is very annoying, there is nothing we can really do to get around this besides using an exposed variable.
+window_api_base* this_ptr = nullptr;
+}  // namespace
+
+window_api::window_api(const window_parameters_t& window_parameters, std::function<void()> window_loop_callback)
+	: window_api_base(std::move(window_loop_callback))
+{
+	// try init glfw
+	if (!glfwInit())
+	{
+		lib_log_e("window_creation: failed to init glfw");
+		assert(false);
+	}
+
+	if (window_parameters.flags & window_flags::window_flag_opengl3)
+	{
+		// Opengl version 3.3
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+
+		// OpenGl core profile
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	}
+	else
+	{
+		// Vulkan and other backends will not use OpenGL
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	}
+
+	// disable msaa
+	glfwWindowHint(GLFW_SAMPLES, 0);
+
+	// remove border and title bar
+	glfwWindowHint(GLFW_DECORATED, !(window_parameters.flags & window_flags::window_flag_no_border));
+
+	// allow resizing
+	glfwWindowHint(GLFW_RESIZABLE, !!(window_parameters.flags & window_flags::window_flag_resizeable));
+
+	// create a new window
+	_glfw_window_ptr = glfwCreateWindow(
+		window_parameters.width,
+		window_parameters.height,
+		window_parameters.window_name.data(),
+		nullptr,
+		nullptr
+		);
+
+	if (!_glfw_window_ptr)
+	{
+		glfwTerminate();
+
+		lib_log_e("window_creation: failed to create glfw window");
+		assert(false);
+	}
+
+	glfwSetWindowPos(_glfw_window_ptr, window_parameters.x_position, window_parameters.y_position);
+
+	glfwMakeContextCurrent(_glfw_window_ptr);
+
+	glfwSwapInterval(1);
+
+	glfwSetInputMode(_glfw_window_ptr, GLFW_STICKY_KEYS, GLFW_TRUE);
+
+	this_ptr = this;
+}
+
+window_api::~window_api()
+{
+	glfwTerminate();
+	this_ptr = nullptr;
+}
+
+#ifndef DEF_LIB_RENDERING_off
+bool window_api::register_renderer(std::unique_ptr<rendering::renderer>& renderer)
+{
+	lib::point2Di window_size = {};
+	glfwGetWindowSize(_glfw_window_ptr, &window_size._x, &window_size._y);
+
+	// initialize renderer using glfw stuff
+	renderer->bind_api(nullptr);
+	renderer->set_window_size(window_size);
+
+	return true;
+}
+#endif
+
+#ifndef DEF_LIB_INPUT_off
+bool window_api::register_input_handler(std::unique_ptr<input::input_handler>& input_handler)
+{
+	// register callback to glfw
+	_input_handler_ptr = input_handler.get();
+	glfwSetKeyCallback(_glfw_window_ptr, input_callback);
+
+	return true;
+}
+#endif
+
+void window_api::window_loop() const
+{
+	lib_log_d("window_creation: starting window loop");
+
+	// run main render thread from current thread
+	while (glfwGetKey(_glfw_window_ptr, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(_glfw_window_ptr) == 0)
+	{
+		glfwPollEvents();
+
+		// callback the parent which will do more stuff
+		_window_loop_callback();
+
+		glfwSwapBuffers(_glfw_window_ptr);
+	}
+
+	lib_log_d("window_creation: destroying window");
+	glfwTerminate();
+}
+
+void window_api::close_window() const
+{
+	glfwSetWindowShouldClose(_glfw_window_ptr, true);
+}
+
+void window_api::focus_window() const
+{
+	glfwMakeContextCurrent(_glfw_window_ptr);
+}
+
+void window_api::input_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+}
--- components/lib_window_creation/lib_window_creation.cpp
+++ components/lib_window_creation/lib_window_creation.cpp
@@ -1,3 +1,117 @@
-//
-// Created by Allan on 18/11/2023.
-//
+#include <lib_window_creation/lib_window_creation.hpp>
+#include <lib_window_creation/window_api.hpp>
+
+#include <cassert>
+
+using namespace lib::window_creation;
+
+void window_creation::init(const window_parameters_t& window_parameters)
+{
+	assert(_init == false);
+
+	const auto window_callback = [&]()
+	{
+		static auto last_frame_time = std::chrono::high_resolution_clock::now();
+
+#ifndef DEF_LIB_RENDERING_off
+		if (_renderer)
+		{
+			if (_render_callback)
+			{
+				_render_callback();
+			}
+
+			_renderer->draw_frame();
+			_renderer->set_frame_time(
+				static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(
+					std::chrono::high_resolution_clock::now() - last_frame_time).count()));
+
+			last_frame_time = std::chrono::high_resolution_clock::now();
+		}
+#endif
+	};
+
+	_init = true;
+	_window_creation_api = std::make_unique<window_api>(window_parameters, window_callback);
+}
+
+void window_creation::destroy()
+{
+	assert(_init == true);
+
+	_init = false;
+	_window_running = false;
+
+	_render_callback = nullptr;
+	_window_creation_api = nullptr;
+
+#ifndef DEF_LIB_RENDERING_off
+	_renderer = nullptr;
+#endif
+
+#ifndef DEF_LIB_INPUT_off
+	_input_handler = nullptr;
+#endif
+}
+
+void window_creation::register_render_callback(std::function<void()> render_callback)
+{
+	assert(_init == true);
+	assert(_renderer != nullptr);
+
+	_render_callback = std::move(render_callback);
+}
+
+void window_creation::run_window_loop()
+{
+	assert(_init == true);
+	assert(_window_running == false);
+
+	_window_running = true;
+	_window_creation_api->window_loop();
+}
+
+void window_creation::close_window() const
+{
+	assert(_init == true);
+
+	_window_creation_api->close_window();
+}
+
+void window_creation::focus_window() const
+{
+	assert(_init == true);
+
+	_window_creation_api->focus_window();
+}
+
+#ifndef DEF_LIB_RENDERING_off
+std::unique_ptr<lib::rendering::renderer>& window_creation::register_renderer(
+	std::unique_ptr<rendering::renderer> renderer)
+{
+	assert(_init == true);
+
+	lib_log_d("window_creation: registered renderer");
+
+	_renderer = std::move(renderer);
+	_window_creation_api->register_renderer(_renderer);
+
+	return _renderer;
+}
+#endif
+
+#ifndef DEF_LIB_INPUT_off
+
+std::unique_ptr<lib::input::input_handler>& window_creation::register_input_handler(
+	std::unique_ptr<input::input_handler> input_handler)
+{
+	assert(_init == true);
+
+	lib_log_d("window_creation: registered input_handler");
+
+	_input_handler = std::move(input_handler);
+	_window_creation_api->register_input_handler(_input_handler);
+
+	return _input_handler;
+}
+#endif
