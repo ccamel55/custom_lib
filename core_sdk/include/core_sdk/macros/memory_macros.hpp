a7dc49ee0dfd2b7c4dda926bc61a6e78876ee681
Allan Ma <allanma31@gmail.com>
2024-02-23 20:18:48 +1300

    remove platform specific memory stuff

--- core_sdk/include/core_sdk/macros/memory_macros.hpp
+++ core_sdk/include/core_sdk/macros/memory_macros.hpp
@@ -1,31 +1,17 @@
 #pragma once
 
-#include <cstdint>
-
-// note: these memory functions only work with x86 windows
-#if _WIN32 || _WIN64
-
 namespace lib
 {
 //! Returns the virtual function found at \a index in the \a class_ptr.
-template <typename t = void*> inline t get_virtual_function(void* class_ptr, size_t index)
+template <typename t = void*> t get_virtual_function(void* class_ptr, size_t index)
 {
 	return (*static_cast<t**>(class_ptr))[index];
 }
 
 //! Call a virtual function found at \a index in the \a class_ptr.
-template <typename t, typename... a> inline t call_virtual_function(void* class_ptr, size_t index, a... args)
+template <typename t, typename... a> t call_virtual_function(void* class_ptr, size_t index, a... args)
 {
 	using fn_sig = t(__thiscall*)(void*, decltype(args)...);
 	return (*static_cast<fn_sig**>(class_ptr))[index](class_ptr, args...);
 }
-
-//! Call a function found at \a fn_address using thiscall.
-template <typename t, typename... a> inline t call_function(void* ecx, uintptr_t fn_address, a... args)
-{
-	using fn_sig = t(__thiscall*)(void*, decltype(args)...);
-	return reinterpret_cast<fn_sig>(fn_address)(ecx, args...);
-}
 }  // namespace lib
-
-#endif
