c6d586ca96111d9b1d75b9dac76aadd6f1e4b207
Allan Ma <allanma31@gmail.com>
2024-06-27 20:21:13 +1200

    update cmake stuff

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -6,18 +6,6 @@ option(
 )
 
 option(
-	LIB_VULKAN_SUPPORT
-	"Whether or not the platform supports Vulkan"
-	OFF
-)
-
-option(
-	LIB_OPENGL_SUPPORT
-	"Whether or not the platform supports OpenGL"
-	OFF
-)
-
-option(
 	LIB_INSTALL_DEPENDENCIES
 	"Allow installation of dependencies"
 	OFF
@@ -30,21 +18,22 @@ option(
 )
 
 # Apply C++ standard options
-set(CMAKE_C_STANDARD 23)
+set(CMAKE_C_STANDARD 17)
 set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-message(STATUS "------------------------------------------")
-message(STATUS "Options:")
+include(Platform)
 
+message(STATUS "------------------------------------------")
+message(STATUS "Properties:")
 message(STATUS "	CMAKE_C_STANDARD: ${CMAKE_C_STANDARD}")
 message(STATUS "	CMAKE_CXX_STANDARD: ${CMAKE_CXX_STANDARD}")
 message(STATUS "	CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
-
+message(STATUS "	CURRENT_ARCH: ${CURRENT_ARCH}")
+message(STATUS "	CURRENT_PLATFORM: ${CURRENT_PLATFORM}")
+message(STATUS "Options:")
 message(STATUS "	LIB_INSTALL_DEPENDENCIES: ${LIB_INSTALL_DEPENDENCIES}")
 message(STATUS "	LIB_INSTALL_MODULES: ${LIB_INSTALL_MODULES}")
-
 message(STATUS "	LIB_ENABLE_TESTS: ${LIB_ENABLE_TESTS}")
-message(STATUS "	LIB_VULKAN_SUPPORT: ${LIB_VULKAN_SUPPORT}")
-message(STATUS "	LIB_OPENGL_SUPPORT: ${LIB_OPENGL_SUPPORT}")
+
---
+++ cmake/Platform.cmake
@@ -0,0 +1,21 @@
+set(CURRENT_ARCH "")
+set(CURRENT_PLATFORM "")
+
+# Note: this doesn't work for all processor, but it should work well enough
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)|(x86_64)|(x64)")
+	set(CURRENT_ARCH "x86")
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "(arm64)|(aarch64)|(arm64)")
+	set(CURRENT_ARCH "arm64")
+else ()
+	set(CURRENT_ARCH "")
+endif ()
+
+if (WIN32)
+	set(CURRENT_PLATFORM "win32")
+elseif (APPLE)
+	set(CURRENT_PLATFORM "darwin")
+elseif (UNIX AND NOT APPLE)
+	set(CURRENT_PLATFORM "linux")
+else ()
+	set(CURRENT_PLATFORM "")
+endif()
